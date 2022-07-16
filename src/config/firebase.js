425ad751aafe9e7be2c8b18f17da7d35b0130e2b
Kishan Virani <kishan@zezamii.com>
2022-07-16 10:26:04 +1000

    added formatting.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'
 
 // Additional firebase module configuration
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyCzncVrTTJzE7uysjEbeANcAIINwnYwLHE',
-	authDomain: 'pets-care-f4e66.firebaseapp.com',
-	projectId: 'pets-care-f4e66',
-	storageBucket: 'pets-care-f4e66.appspot.com',
-	messagingSenderId: '394440874026',
-	appId: '1:394440874026:web:9ed9719c099e2f37b74b53'
-};
+    apiKey: 'AIzaSyCzncVrTTJzE7uysjEbeANcAIINwnYwLHE',
+    authDomain: 'pets-care-f4e66.firebaseapp.com',
+    projectId: 'pets-care-f4e66',
+    storageBucket: 'pets-care-f4e66.appspot.com',
+    messagingSenderId: '394440874026',
+    appId: '1:394440874026:web:9ed9719c099e2f37b74b53',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
-export default app;
+export default app
