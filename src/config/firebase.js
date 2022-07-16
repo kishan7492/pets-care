218b3bfc84cdd2a599649165a53319cbc70a72f1
Kishan Virani <kishan@zezamii.com>
2022-07-16 10:15:39 +1000

    init firebase app.

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCzncVrTTJzE7uysjEbeANcAIINwnYwLHE',
+	authDomain: 'pets-care-f4e66.firebaseapp.com',
+	projectId: 'pets-care-f4e66',
+	storageBucket: 'pets-care-f4e66.appspot.com',
+	messagingSenderId: '394440874026',
+	appId: '1:394440874026:web:9ed9719c099e2f37b74b53'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
