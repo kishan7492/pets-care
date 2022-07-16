fe25da3d8dd66b9a5168662620295f6985ccd6ac
Kishan Virani <kishan@zezamii.com>
2022-07-16 10:20:16 +1000

    Added ant design and organised the Components

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Additional firebase module configuration
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
